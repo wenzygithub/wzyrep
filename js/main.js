0ed9b43d4a62900d74dd648774edc3f5c340b8ba
wenzygithub <37325268+wenzygithub@users.noreply.github.com>
2018-06-18 12:13:49 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function(){
+
+	prepareIcon();
+	
+});
+
+$(window).resize(function() { 
+	
+	prepareIcon();
+
+});
+function prepareIcon(){
+
+	var width = $(".social-icon").width();
+	$(".social-icon").css("height",width+"px");
+	$(".social-icon").css("font-size",0.5*width+"px");
+	$(".social-icon").css("line-height",width+"px");
+	
+}
+
